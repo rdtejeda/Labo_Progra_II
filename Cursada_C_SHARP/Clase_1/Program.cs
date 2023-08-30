5228dd8c6573851322038a232c23cdb1ab2d81e5
Roberto D. Tejeda <86858522+rdtejeda@users.noreply.github.com>
2023-08-30 15:23:26 -0300

    30Agosto_15:23

---
+++ Cursada_C_SHARP/Clase_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace Clase_1
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Entry point
+        {
+            Console.WriteLine("Hello, World!");//print - console.log() - printf
+        }
+    }
+}
