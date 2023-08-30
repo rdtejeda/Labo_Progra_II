1a09ef3754b8d33cf404019a8e8d7ae12cb5c2ec
Roberto D. Tejeda <86858522+rdtejeda@users.noreply.github.com>
2023-08-30 17:55:56 -0300

    23Ago23_17:55

--- Cursada_C_SHARP/Clase_1/Program.cs
+++ Cursada_C_SHARP/Clase_1/Program.cs
@@ -1,12 +1,54 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Text;
 
 namespace Clase_1
 {
     internal class Program
     {
-        static void Main(string[] args) //Entry point otra versión
+        static void Main(string[] args) //Entry point otra versi
         {
-            Console.WriteLine("Hello, World!");//print - console.log() - printf
-        }
+            //         // Console.WriteLine("Hello, World!");//print - console.log() - printf
+            //         int a;
+            //         a = 4;
+            //         a = int.MinValue;
+            //         Console.WriteLine(a);
+            //         if (a == int.MinValue)
+            //         {
+            //             bool valor = true;
+            //             Console.WriteLine(valor);
+            //         }
+            //         float numero = 15;
+            //         //int nuemr2= 3.5; no sirve conversion implicita
+            //int nuemro = (int)3.1; //conversion explicta si permitida
+            //Console.WriteLine(nuemro);
+            string suNombre;
+            int edad;
+
+           
+            try
+            {
+                Console.WriteLine("Ingrese su nombre");			
+			    suNombre = Console.ReadLine();
+
+			    Console.WriteLine("Ingrese su edad");
+                //edad = Console.ReadLine().Length; 
+                //"35".Length;
+                edad = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Su nombre es " + suNombre);// no usar esto
+                Console.WriteLine($"Su nombre es {suNombre}");
+                Console.WriteLine("Su nombre es {0} y tenes {1} años", suNombre, edad);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ingese un numero");
+            }
+            finally
+            {
+                Console.WriteLine("Final del programa");
+            }
+			
+		}
     }
 }
---
+++ Python/Python.py
@@ -0,0 +1,15 @@
+a=4
+print(Type(a))
+print(id(a))
+
+a=8
+print(Type(a))
+print(id(a))
+
+if a==4:
+valor = True
+print(type(a))
+print (valor) #una linae
+'''
+bloque
+'''
